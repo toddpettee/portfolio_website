dd8f18bbcfdf00c49303f7d0523e32911daf56f1
tpettee <pettee@gmail.com>
2020-04-05 22:30:11 -0700

    new menu

--- js/main.js
+++ js/main.js
@@ -9,6 +9,17 @@ function toggleMenu() {
 	}
 }
 
+function hideBar() {
+	const hideBar = document.querySelector('.header__nav-icon');
+	if (hideBar.classList.contains('close')) {
+		hideBar.classList.remove('close');
+		hideBar.classList.add('open');
+	} else {
+		hideBar.classList.remove('open');
+		hideBar.classList.add('close');
+	}
+}
+
 function hideMenu() {
 	const hideMenu = document.querySelector('.header__nav');
 	if (hideMenu.style.display === 'block') {
@@ -17,3 +28,20 @@ function hideMenu() {
 		hideMenu.style.display = 'block';
 	}
 }
+
+// https://codepen.io/AJamesL/pen/MXmvZp?page=5
+// var menuToggle = document.querySelector("#menu-toggle");
+// var activeElements = document.querySelectorAll(".active-element");
+// var toggledMenu = menuToggle.addEventListener("click", function(){
+//      // forEach is not supported in IE11
+//   // activeElements.forEach(function(e){
+//   //     e.classList.toggle("active");
+//   // });
+//      for(var activated = 0; activated < activeElements.length; activated++){
+//           activeElements[activated].classList.toggle("active");
+//      }
+// })
+
+// https://zellwk.com/blog/css-transitions/
+// const button = document.querySelector('.header__nav')
+// button.addEventListener('click', _ => header__nav-icon.classList.toggle('open'))
